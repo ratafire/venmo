8d29f4022ebb9d2fefc90c0230580fe49f202107
Fari Liang <fariliang@users.noreply.github.com>
2015-04-27 22:46:30 -0400

    Update pay.rb

--- lib/venmo/pay.rb
+++ lib/venmo/pay.rb
@@ -1,32 +1,33 @@
 module Venmo
   module Pay
-    def pay_by_email(email, amount, note, access_token, privacy)
+    def pay_by_email(email, amount, note)
       # -d email= \
-      @send_hash= common(amount,note,access_token,privacy, { :email => email})
+      @send_hash= common(amount,note, { :email => email})
       core_pay
     end
 
-    def pay_by_user_id(user_id, amount, note, access_token, privacy)
+    def pay_by_user_id(user_id, amount, note)
       # -d user_id= \
-      @send_hash= common(amount,note,access_token,privacy, {:user_id => user_id})
+      @send_hash= common(amount,note, {:user_id => user_id})
       core_pay
     end
 
-    def pay_by_phone_number(phone_number, amount, note, access_token, privacy)
+    def pay_by_phone_number(phone_number, amount, note)
       # -d phone= \
-      @send_hash = common(amount,note,access_token,privacy, {:phone=> phone_number})
+      @send_hash = common(amount,note, {:phone=> phone_number})
       core_pay
     end
 
     protected
-    def common(new_amount, new_note, access_token, privacy, merge_in)
+    def common(new_amount, new_note, merge_in)
       {
        amount: new_amount,
        note: new_note,
-       access_token: access_token,
-       audience: privacy,
+       access_token: Venmo.access_token,
+       audience: (Venmo.privacy || "private")
       }.merge(merge_in)
     end
+
 
     def core_pay
       h = @send_hash.collect{|k,v| "#{k.to_s}=#{v.respond_to?(:gsub) ? CGI::escape(v) : v}"}.join('&')
